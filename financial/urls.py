c0172530bdde235c9e532b80cd0b90e1cfb1e891
Shuhnaty <shuhnaty@gmail.com>
2022-11-16 10:58:04 +0300

    add forms file

--- financial/urls.py
+++ financial/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import financial_list
+from . import views
 
 
 urlpatterns = [
-    path('financial/',financial_list, name='financial_list' ),
+    path('financial/',views.catch_receipt_list, name='catch_receipt_list' ),
+    path('financial/details/<int:pk>',views.catch_receipt_details, name='catch_receipt_details' ),
+    path('financial/add',views.add_catch_receipt, name='add_catch_receipt' ),
+    path('financial/edit/<int:pk>',views.edit_catch_receipt, name='edit_catch_receipt' ),
+    path('financial/delete/<int:pk>',views.delete_catch_receipt, name='delete_catch_receipt' ),
 ]
--- financial/views.py
+++ financial/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from .models import  CatchReceipt
+from .models import  Invoices
+
+
+# financial
+#catch_receipt
+# financials
 # Create your views here.
-def financial_list(request):
-    return render(request, 'financial/financial-list.html')
+def catch_receipt_list(request):
+    catch_receipt = CatchReceipt.objects.all()
+    return render(request, 'financial/catch_receipt_list.html',{'catch_receipt':catch_receipt})
+
+def catch_receipt_details(request, pk):
+    catch_receipt = get_object_or_404(CatchReceipt, pk=pk)
+    return render(request, 'financial/catch_receipt_details.html',{'catch_receipt':catch_receipt})
+
+def add_catch_receipt(request):
+    if request.method == 'POST':
+        catch_receipt_form = CatchReceipt(request.POST, request.FILES)
+        if catch_receipt_form.is_valid():
+            catch_receipt_form.save()
+            return redirect('catch_receipt_list')
+    else:
+        catch_receipt_form = CatchReceipt()
+
+    return render(request, 'financial/catch_receipt_add_edit.html',{'catch_receipt_form':catch_receipt_form})
+
+
+def edit_catch_receipt(request, pk):
+    catch_receipt = get_object_or_404(CatchReceipt, pk=pk)
+    if request.method == 'POST':
+        catch_receipt_form = CatchReceipt(request.POST, instance=catch_receipt)
+        if catch_receipt_form.is_valid():
+            catch_receipt_form.save()
+            return redirect('catch_receipt_list')
+    else:
+        catch_receipt_form = CatchReceipt(instance=catch_receipt)
+
+    return render(request, 'financial/catch_receipt_add_edit.html',{'catch_receipt_form':catch_receipt_form})
+
+def delete_catch_receipt(request, pk):
+    catch_receipt = get_object_or_404(CatchReceipt, pk=pk)
+    catch_receipt.delete()
+    return redirect('catch_receipt_list')
